1d23c006fb6e7d237d270dc8221a13aee11a6a70
Nguyễn Nhựt <51232550+justonelife@users.noreply.github.com>
2025-10-03 14:43:09 +0700

    chore: remove unused code

--- projects/lib-overlay/src/lib/directives/base-alert.directive.ts
+++
@@ -1,7 +0,0 @@
-import { Directive } from '@angular/core';
-import { IAlertComponent } from '../components/alert/alert.component';
-
-@Directive({
-  selector: '[libBaseAlert]',
-})
-export abstract class LibBaseAlert implements IAlertComponent {}
